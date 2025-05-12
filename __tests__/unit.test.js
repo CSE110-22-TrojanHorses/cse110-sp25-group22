4ba7cf9a09e27a20245fe04a066d83f65360e053
adahe8 <ada.he0908@gmail.com>
2025-05-11 22:14:15 -0700

    tester code and pipeline set up

---
+++ __tests__/unit.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+// unit.test.js
+
+import { add, subtract, multiply, divide } from "..test-script.js";
+
+test("should test add", () => {
+  expect(add(2, 4)).toBe(6);
+});
+
+test("should test subtract", () => {
+  expect(subtract(2, 2)).toBe(0);
+});
+
+test("should test multiply", () => {
+  expect(multiply(2, 2)).toBe(4);
+});
+
+test("should test divide", () => {
+  expect(divide(2, 2)).toBe(1);
+});
