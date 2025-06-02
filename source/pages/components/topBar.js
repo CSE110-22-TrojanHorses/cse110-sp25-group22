57d5cee86a8f2ffb894ff7c08874e89bf81ea00b
Ayaka Kawano <ayaka.kawano172@gmail.com>
2025-06-02 16:11:04 -0700

    added save functionality to save button

--- source/pages/components/topBar.js
+++ source/pages/components/topBar.js
@@ -28,8 +28,48 @@ class TopBar extends HTMLElement {
         break;
       case 1:
         button.innerHTML = `<img src="../../assets/icons/top-bar-icons/disk.png" alt="Diagram">`;
-        button.addEventListener("click", function () {
-          alert("Save clicked!");
+        button.addEventListener("click", async function () {
+          if (document.querySelector("greeting-card")) {
+            // get card DOM
+            const card = document.querySelector("greeting-card").shadowRoot;
+
+            // get inside and outside of cards
+            const inside = card.querySelector(".inside");
+            const outside = card.querySelector(".outside");
+
+            // array of element types that are used
+            const elementTypes = ["img", "input", "h2", "p"];
+
+            // storage object to stringify
+            const storage = {};
+            storage.insideElements = [];
+            storage.outsideElements = [];
+
+            // store arrays for each element type and get the outerHTML for each element
+            for (let i = 0; i < elementTypes.length; i++) {
+              const insideElementList = Array.from(inside.getElementsByTagName(elementTypes[i]));
+              for (let j = 0; j < insideElementList.length; j++) {
+                if (elementTypes[i] == "input") {
+                  insideElementList[j] = [insideElementList[j].outerHTML, insideElementList[j].value]; 
+                } else {
+                  insideElementList[j] = insideElementList[j].outerHTML;
+                }
+              }
+              const outsideElementList = Array.from(outside.getElementsByTagName(elementTypes[i]));
+              for (let j = 0; j < outsideElementList.length; j++) {
+                if (elementTypes[i] == "input") {
+                  outsideElementList[j] = [outsideElementList[j].outerHTML, outsideElementList[j].value]; 
+                } else {
+                  outsideElementList[j] = outsideElementList[j].outerHTML;
+                }
+              }
+              storage.insideElements.push(insideElementList);
+              storage.outsideElements.push(outsideElementList);
+            }
+
+            // adds data to local storage
+            localStorage.setItem("card data", JSON.stringify(storage));
+          }
         });
         button.className = "save";
         break;
