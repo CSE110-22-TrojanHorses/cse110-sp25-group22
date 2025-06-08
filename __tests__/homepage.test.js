4f754fd015482d23157fa813b45cf24c3b07de7d
adahe8 <ada.he0908@gmail.com>
2025-06-08 12:53:01 -0700

    updated test descriptions

--- __tests__/homepage.test.js
+++ __tests__/homepage.test.js
@@ -3,11 +3,12 @@ describe("Basic user flow for website", () => {
   // First visit website
   beforeEach(async () => {
     await page.goto(
-      "https://cse110-22-trojanhorses.github.io/cse110-sp25-group22/pages/home_page/homepage.html");
-      await page.evaluate(() => localStorage.clear());
+      "https://cse110-22-trojanhorses.github.io/cse110-sp25-group22/pages/home_page/homepage.html"
+    );
+    await page.evaluate(() => localStorage.clear());
   });
 
-let testCard = `{
+  let testCard = `{
   "leftElements": [["INPUT", "<input type=\\"text\\" placeholder=\\"Left Page\\" class=\\"page left\\">", ""]],
   "rightElements": [["INPUT", "<input type=\\"text\\" placeholder=\\"Feel free to write your custom contents...\\" class=\\"page right\\">", ""]],
   "backElements": [["INPUT", "<input type=\\"text\\" placeholder=\\"Back Cover\\" class=\\"page back\\">", ""]],
@@ -19,8 +20,7 @@ let testCard = `{
   "time": "Last Sync: 6/6/2025 @ 23:9:47"
 }`;
 
-
-    it("NavBar ‘home’ button stays on homepage", async () => {
+  it("NavBar ‘home’ button stays on homepage", async () => {
     const navBar = await page.$("nav-bar");
     const shadow = await (await navBar.getProperty("shadowRoot")).asElement();
     const homeBtn = await shadow.$("#home");
@@ -34,37 +34,37 @@ let testCard = `{
   });
 
   it("homepage card load with card", async () => {
-   await page.evaluate((cardStr) => {
-  localStorage.clear();
-  localStorage.setItem("test card", cardStr);
-  localStorage.setItem("current card", "test card");
-}, testCard);
+    await page.evaluate((cardStr) => {
+      localStorage.clear();
+      localStorage.setItem("test card", cardStr);
+      localStorage.setItem("current card", "test card");
+    }, testCard);
 
     const navBar = await page.$("nav-bar");
     const shadow = await navBar.getProperty("shadowRoot");
     const homeButton = await shadow.$("#home");
     await Promise.all([
-    page.waitForNavigation({ waitUntil: "networkidle0" }), // or "domcontentloaded"
-   homeButton.click(),
-  ]);
+      page.waitForNavigation({ waitUntil: "networkidle0" }), // or "domcontentloaded"
+      homeButton.click(),
+    ]);
     const storedValue = await page.evaluate(() => {
-    return localStorage.getItem('current card');
-  });
-  expect(storedValue).toBe("test card");
+      return localStorage.getItem("current card");
+    });
+    expect(storedValue).toBe("test card");
   }, 6000);
   /*
-  it.skip("homepage card load with no card", async () => {
+  it.skip("The homepage should load with no cards and show default message when there are no cards", async () => {
     localStorage.clear();
   });
 
-  it.skip("homepage card double click", async () => {});
+  it.skip("Confirm that edit/delete menu shows up with homepage card double click", async () => {});
 
-  it.skip("homepage card edit", async () => {});
+  it.skip("Confirm that homepage card edit takes you to editor", async () => {});
 
-  it.skip("homepage card delete", async () => {});
+  it.skip("Confirm that homepage card delete removes the card", async () => {});
 
-  it.skip("homepage card delete all", async () => {});
+  it.skip("Confirm that page is empty when all homepage cards deleted all", async () => {});
 
-  it.skip("add button click", async () => {});
+  it.skip("Clicking the add button takes you to the editor page", async () => {});
   */
 });
