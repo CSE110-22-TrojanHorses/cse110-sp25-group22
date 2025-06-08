ba247ef41b7126dcb6a65f1f6e0a375019958364
Andrew <anl139@ucsd.edu>
2025-06-08 15:06:23 -0700

    fixed i hope

--- __tests__/editorPage.test.js
+++ __tests__/editorPage.test.js
@@ -1,15 +1,31 @@
-
+/* global page */
 describe('Basic user flow for website', () => {
     // First visit website
     beforeEach(async () => {
-        await page.goto('https://cse110-22-trojanhorses.github.io/cse110-sp25-group22/pages/home_page/homepage.html');
+        await page.goto('https://cse110-22-trojanhorses.github.io/cse110-sp25-group22/pages/editor_page/index.html');
     });
 
     // Save button functionality
-    it.skip('save button no edits', async () => {
+    it('save button no edits', async () => {
+        await page.goto("https://cse110-22-trojanhorses.github.io/cse110-sp25-group22/pages/editor_page/index.html",{ waitUntil: "networkidle0" });
+        await page.waitForSelector("top-bar");
+        const saveBtn = await page.evaluateHandle(() => {
+            const topBar = document.querySelector("top-bar");
+            if (!topBar) return null;
+            const shadow = topBar.shadowRoot;
+            return shadow?.querySelector(".save") || null;
+        });
+        if (!saveBtn) {
+            throw new Error("Save button not found in shadow DOM.");
+        }
+        await Promise.all([
+            saveBtn.click(),
+        ]);
+        const unchangedCard = await page.evaluate(() => localStorage.getItem("current card"));
+        expect(unchangedCard).toBe("card 1");
           
-    });
-
+    }, 60000);
+/*
     it.skip('save button with edits', async() => {
 
     });
@@ -37,4 +53,5 @@ describe('Basic user flow for website', () => {
     it.skip('load card data when edit button is clicked', async() => {
 
     });
+    */
 })
--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -2,6 +2,7 @@
 describe('my first test with jest-puppeteer', () => {
   beforeAll(async () => {
     await page.goto('https://cse110-22-trojanhorses.github.io/cse110-sp25-group22/pages/home_page/homepage.html');
+    await page.evaluate(() => localStorage.clear());
   });
 
   test('Create card and store content in localStorage', async () => {
